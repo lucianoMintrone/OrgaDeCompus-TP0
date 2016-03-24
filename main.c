7cd23b6d09779e08abdbd9fee0f0df049507fdb3
Agustina <agustina.barbetta@gmail.com>
2016-03-24 16:40:52 -0300

    Flag recognition implemented

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdbool.h>
 #include <stdio.h>
-//#include "ascii264.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+//#include "ascii_64.h"
 
 #define HELP_LENGTH 297
+#define VERSION 1.0
+
+void show_version() {
+    printf("v%f\n", VERSION);
+}
 
 size_t fsize(FILE* fp) {
   size_t size;
@@ -27,10 +35,41 @@ void show_help() {
 }
 
 int main (int argc, char *argv[]) {
-  //bool version, help, output, input, decode;
+  bool output, input, decode;
+  output = input = decode = false;
+  char *input_file, *output_file;
+  int flag = 0;
 
+  while ((flag = getopt(argc, argv,"Vho:i:d")) != -1) {
+    switch (flag) {
+      case 'V' :
+        show_version();
+        break;
+      case 'h' :
+        show_help();
+        break;
+      case 'o' :
+        output_file = optarg;
+        output = true;
+        break;
+      case 'i' :
+        input_file = optarg;
+        input = true;
+        break;
+      case 'd' :
+        decode = true;
+        break;
+      default:
+        show_help();
+        return EXIT_FAILURE;
+    }
+  }
 
+  if (input && output && decode) {
+    //64_2_ascii(input_file, output_file);
+  } else if (input && output) {
+    //ascii_2_64(input_file, output_file);
+  }
 
-
-  return 0;
+  return EXIT_SUCCESS;
 }
