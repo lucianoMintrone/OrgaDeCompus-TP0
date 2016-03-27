6029e93fb1565d1ef343aa8a5127ac78c3caa9ca
Agustina Barbetta <agustina.barbetta@gmail.com>
2016-03-27 20:49:01 -0300

    changed whiles to fors

--- handlers/file_handler.c
+++ handlers/file_handler.c
@@ -7,12 +7,10 @@
 // En charArray aloca 3 caracteres de un .txt, posiciones 1 2 3.
 // La posicion 0 esta reservada para la longitud logica.
 void getArrayOfCaracters(FILE* fp, char* charArray) {
-    int i = 0;
-       
-    while(!feof(fp) && i != 3){     
-        charArray[i + 1] = fgetc(fp);        
-        i++;
-    }
+	
+    for (int i = 1; !feof(fp) && i < 4; i++)     
+        charArray[i] = fgetc(fp);
+
     verifyArrayData(&charArray);
 }
 
@@ -20,25 +18,23 @@ void getArrayOfCaracters(FILE* fp, char* charArray) {
 // Remueve el -1 que indica EOF.
 // Y en la posicion cero setea la longitud logica, cantidad de valores validos.
 void verifyArrayData(char* charArray) {
-    int i = 1;
     bool loop = true;
     charArray[0] = 3;
     
-    while ((i <= 3) && loop) {
-        
+    for (i = 1; i <= 4 && loop; i++) {
         if (charArray[i] == -1) {
             loop = false;
             charArray[0] = i - 1;
         }
-        i++;
     }
 }
 
 void writeArray(FILE* fp, char* array) {
+	/*
     char string[4];
     
     for (int i = 0; i < 4; i++)
 		string[i] = array[i];
-    
-    fprintf(fp, "%s", string); 
+    */
+    fprintf(fp, "%s", array); // No es lo mismo?
 }
